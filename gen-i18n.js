14e34ee3b3729b924be9c117186717271078dbd7
Sean <sconnolly17@gmail.com>
2016-05-21 06:07:34 +0000

    Implement json support

--- gen-i18n.js
+++ gen-i18n.js
@@ -1,10 +1,22 @@
 var fs = require('fs');
+var slug = require('slug');
 
 var args = process.argv.slice(2);
 var sourceFile = args[0];
+var destinationFile = args[1];
 
-console.log(sourceFile);
+console.log("Reading from " + sourceFile);
 
-fs.readFile(sourceFile, 'utf8', function(err, data) {
-  console.log(typeof data);
+var strings = fs.readFileSync(sourceFile).toString().split("\n");
+
+console.log("Generating keys for " + strings.length + " strings");
+
+var keyedStrings = strings.map(function(line) {
+  var obj = {};
+  obj[slug(line, {lower: true, replacement: '_'}).substring(0, 25)] = line;
+  return obj;
 });
+
+fs.writeFileSync(destinationFile, JSON.stringify(keyedStrings, null, 2), 'utf8');
+
+console.log("Written to " + destinationFile);
