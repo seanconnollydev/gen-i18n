c2ccc84223a6fc30e0f46cc26673b5e528ee279f
Sean Connolly <sconnolly17@gmail.com>
2016-05-25 22:28:13 -0600

    More efficiently read and write files

--- replace.js
+++ replace.js
@@ -1,35 +1,35 @@
 #! /usr/bin/env node
 
 var fs = require('fs');
-var replace = require('replace');
+var glob = require('glob');
 
 var args = process.argv.slice(2);
 var keyFile = args[0];
-var targetPath = args[1];
-
-console.log("Key file: " + keyFile);
+var inputGlob = args[1];
 
 var keyedStrings = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
 var keys = Object.keys(keyedStrings);
 
-keys.forEach(function(key) {
-  var str = keyedStrings[key];
-  
-  /*
-    <p>Some text</p>
-    <h3>Some text</h3>
-    <md-button class="blah">Some text</md-button>
-  */
-  var regex = "(<.+>)" + str + "(<\/.+>)";
-  
-  replace({
-    regex: regex,
-    replacement: "$1{{'" + key + "' | translate}}$2", //{{'nav_profile' | translate}}
-    paths: ['.'],
-    include: targetPath,
-    recursive: true,
-    silent: false
-  });
-  
-});
+glob(inputGlob, {}, function(er, files) {
+  files.forEach(function(file) {
+    fs.readFile(file, 'utf8', function(err, contents) {
 
+      keys.forEach(function(key) {
+
+        /*
+          <p>Some text</p>
+          <h3>Some text</h3>
+          <md-button class="blah">Some text</md-button>
+          <h5> Remember leading and trailing spaces </h5>
+        */
+        contents = contents.replace(
+          new RegExp("(<.+>\\s*)" + keyedStrings[key] + "(\\s*<\/.+>)"),
+          "$1{{'" + key + "' | translate}}$2");
+      });
+
+      fs.writeFile(file, contents, function() {
+        console.log(file);
+      });
+    });
+  });
+});
