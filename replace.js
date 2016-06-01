212e2962293330bc773964d984ee0091ee501020
Sean <sconnolly17@gmail.com>
2016-06-01 05:08:17 +0000

    Safer writes

--- replace.js
+++ replace.js
@@ -12,24 +12,20 @@ var keys = Object.keys(keyedStrings);
 
 glob(inputGlob, {}, function(er, files) {
   files.forEach(function(file) {
-    fs.readFile(file, 'utf8', function(err, contents) {
-
-      keys.forEach(function(key) {
-
-        /*
-          <p>Some text</p>
-          <h3>Some text</h3>
-          <md-button class="blah">Some text</md-button>
-          <h5> Remember leading and trailing spaces </h5>
-        */
-        contents = contents.replace(
+    var contents = fs.readFileSync(file, 'utf8');
+    keys.forEach(function(key) {
+      /*
+        <p>Some text</p>
+        <h3>Some text</h3>
+        <md-button class="blah">Some text</md-button>
+        <h5> Remember leading and trailing spaces </h5>
+      */
+      contents = contents.replace(
           new RegExp("(<.+>\\s*)" + keyedStrings[key] + "(\\s*<\/.+>)"),
           "$1{{ '" + key + "' | translate }}$2");
-      });
-
-      fs.writeFile(file, contents, function() {
-        console.log(file);
-      });
     });
+
+    fs.writeFileSync(file, contents);
+    console.log(file);
   });
 });
