5827102ff8df142a8d647e7a4c360bec1d751a84
Sean Connolly <sconnolly17@gmail.com>
2016-06-12 11:51:58 -0400

    Attempting to improve extract accuracy

--- src/extract.js
+++ src/extract.js
@@ -5,10 +5,12 @@ var glob = require('glob');
 module.exports = function extract(options) {
   var inputGlob = options.path;
   var destinationFile = options.dest;
+  var keyFile = options.keys;
 
-  var htmlTextRegex = /<.+>(.+)<\/.+>/g; // <h3>Word</h3>
+  var htmlTextRegex = /<.+>(.+?)<\/.+>/g; // <h3>Word</h3>
   var iconRegex = /<i.*|md-icon.*>(.+)<\/i.*|md-icon.*>/; //but not <i class="material-icons">check</i> or <md-icon class="material-icons">check</md-icon>
-  var angularFilterRegex = /{{.+}}/; // {{angularMessage}}, {{ 'word' | translate }}
+  var angularExpressionRegex = /{{.+}}/; // {{angularMessage}}, {{ 'word' | translate }}
+  var keys = Object.keys(JSON.parse(fs.readFileSync(keyFile, 'utf8')));
   var strings = [];
 
   glob(inputGlob, {}, function(er, files) {
@@ -17,8 +19,15 @@ module.exports = function extract(options) {
       var contents = fs.readFileSync(file, 'utf8');
       var match = htmlTextRegex.exec(contents);
       while (match != null) {
-        if(!iconRegex.test(match[0]) && !angularFilterRegex.test(match[1]) && strings.indexOf(match[1]) === -1) {
-          strings.push(match[1]);  
+        var tagWithText = match[0]; // <span><span translate>i_am_a_phrase</span></span>
+        var innerText = match[1]; // i_am_a_phrase</span>
+        if(
+          strings.indexOf(innerText) === -1 && // The string was not already added to the output
+          keys.indexOf(innerText) === -1 && // The string does not already exist as a key in the key file
+          !angularExpressionRegex.test(innerText) && // The string is not an Angular expression
+          !iconRegex.test(tagWithText)) // The string is not wrapped in an icon tag
+        {
+          strings.push(innerText);
         }
         match = htmlTextRegex.exec(contents);
       }
