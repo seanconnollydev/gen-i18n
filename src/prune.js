499b2e8a7c5cd5217d1f1ae5ad441bd2fcf7673d
Sean Connolly <sconnolly17@gmail.com>
2016-06-17 09:49:18 -0600

    Preserve order of keys when pruning

--- src/prune.js
+++ src/prune.js
@@ -7,7 +7,7 @@ module.exports = function(options) {
   var inputGlob = options.path;
 
   console.log("Reading keys from " + keyFile);
-  var sourceKeys = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
+  var originalKeys = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
 
   var used = {};
 
@@ -15,14 +15,21 @@ module.exports = function(options) {
     files.forEach(function(file) {
       var contents = fs.readFileSync(file, 'utf8');
 
-      for(var key in sourceKeys) {
+      for(var key in originalKeys) {
         if(!used[key] && contents.includes(key)) {
-          used[key] = sourceKeys[key];
+          used[key] = originalKeys[key];
         }
       }
     });
 
-    fs.writeFileSync(keyFile, JSON.stringify(used, null, 2), 'utf8');
+    var newKeys = {};
+    for (var key in originalKeys) {
+      if(used[key]) {
+        newKeys[key] = originalKeys[key];
+      }
+    }
+
+    fs.writeFileSync(keyFile, JSON.stringify(newKeys, null, 2), 'utf8');
     console.log("Written to " + keyFile);
   });
 }
